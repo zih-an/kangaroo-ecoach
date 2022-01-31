811646c6dad4cfe243a869c7fc16424e29035603
zih-an <zih_an@163.com>
2022-01-31 15:58:13 +0800

    add reports tab content template

--- ecoach-frontend/components/PostTab.js
+++ ecoach-frontend/components/PostTab.js
@@ -1,6 +1,18 @@
 import React from "react";
-import { Text } from "@ui-kitten/components";
+import { ScrollView } from "react-native";
+import { Layout, Text } from "@ui-kitten/components";
+import StarGroup from "./StarGroup";
 
-export default function HLMomentTab() {
-  return <Text>post</Text>;
+export default function HLMomentTab(props) {
+  return (
+    <Layout
+      style={{
+        height: "93%",
+        justifyContent: "flex-start",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ height: 200 }}>雷达图放在这里</Text>
+    </Layout>
+  );
 }
